aff4792d1dcc199eee811b9f655877ea0a7b573b
Anjihee <jihee5100@sookmyung.ac.kr>
2023-08-06 03:57:45 +0900

    [0806] 지희: 마이페이지 페이지 이동 구현

--- app/src/main/java/com/surround2023/surround2023/mypage/MypageActivity.kt
+++ app/src/main/java/com/surround2023/surround2023/mypage/MypageActivity.kt
@@ -4,37 +4,76 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.surround2023.surround2023.R
-import com.surround2023.surround2023.databinding.ActivityHomeBinding
+import com.surround2023.surround2023.community_log.CommunityLogActivity
+import com.surround2023.surround2023.databinding.ActivityMypageBinding
 import com.surround2023.surround2023.home.HomeActivity
+import com.surround2023.surround2023.market_log.MarketLogJoinActivity
 import com.surround2023.surround2023.market_log.MarketLogOpenActivity
+import com.surround2023.surround2023.posting.MarketPostingActivity
+import com.surround2023.surround2023.set_location.SetLocationActivity
+import com.surround2023.surround2023.user_login_join.LogoutActivity
 
 class MypageActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityHomeBinding
+    private lateinit var binding: ActivityMypageBinding
 
     //하단 Nav 와 관련된 변수
     private lateinit var bottomNavView: BottomNavigationView
 
-//    val mypagebtn1: Button = findViewById(R.id.mypagebtn1)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mypage)
-
-
-//        mypagebtn1.setOnClickListener {
-//            val intent = Intent(this, MarketLogOpenActivity::class.java)
-//            startActivity(intent)
-//        }
-//
-
-
-        binding= ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityMypageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //        ---------------------BottomNavigationView에 대한 기능 ---------------
+
+        // mypagebtn1 버튼 클릭시 내가 진행한 공구
+        val mypagebtn1: Button = findViewById(R.id.mypagebtn1)
+        mypagebtn1.setOnClickListener {
+            val intent = Intent(this, MarketLogOpenActivity::class.java)
+            startActivity(intent)
+        }
+
+        // mypagebtn2 버튼 클릭시 내가 참여 공구
+        val mypagebtn2: Button = findViewById(R.id.mypagebtn2)
+        mypagebtn2.setOnClickListener {
+            val intent = Intent(this, MarketLogJoinActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+        //커뮤니티 활동관리로 이동
+        val communitylogtext: TextView = findViewById(R.id.communitylogtext)
+        communitylogtext.setOnClickListener {
+            val intent = Intent(this, CommunityLogActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        //동네 설정으로 이동
+        val setlocationtext: TextView = findViewById(R.id.setlocationtext)
+        setlocationtext.setOnClickListener {
+            val intent = Intent(this, SetLocationActivity::class.java)
+            startActivity(intent)
+        }
+
+
+//        //로그아웃으로 이동
+//        val logouttext: TextView = findViewById(R.id.logouttext)
+//        logouttext.setOnClickListener {
+//            val intent = Intent(this, LogoutActivity::class.java)
+//            startActivity(intent)
+//        }
+
+
+
+
+
+
+        //하단바 기능
         bottomNavView = binding.bottomNavView
         bottomNavView.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
@@ -45,18 +84,20 @@ class MypageActivity : AppCompatActivity() {
                     startActivity(intent)
                     true
                 }
-                R.id.menu_favorite ->{
+                R.id.menu_favorite -> {
                     //즐겨찾기로 이동
                     true
                 }
 
-                R.id.menu_addPost ->{
+                R.id.menu_addPost -> {
                     //공동구매 글쓰기로 이동
+                    val intent = Intent(this, MarketPostingActivity::class.java)
+                    startActivity(intent)
                     true
                 }
 
-                R.id.menu_mypage ->{
-                    //마이페이지로 이동
+                R.id.menu_mypage -> {
+                    //마이페이지로 이동 (현재 액티비티이므로 아무 동작 없음)
                     true
                 }
                 // 다른 메뉴 아이템에 대한 처리 추가 (필요에 따라 다른 Activity로 이동할 수 있음)
@@ -64,5 +105,4 @@ class MypageActivity : AppCompatActivity() {
             }
         }
     }
-
 }
