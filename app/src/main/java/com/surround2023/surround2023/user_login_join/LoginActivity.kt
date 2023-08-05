49201ecf3f6b4142999e813c0f4dba6ee83a8be4
lehiewl <uniowl10801@gmail.com>
2023-08-06 00:30:12 +0900

    [0805] 윤지:카카오 로그인, firebase 연동

--- app/src/main/java/com/surround2023/surround2023/user_login_join/LoginActivity.kt
+++ app/src/main/java/com/surround2023/surround2023/user_login_join/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.google.firebase.auth.FirebaseAuth
@@ -30,20 +31,20 @@ class LoginActivity : ComponentActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        val join_intent = Intent(this, JoinActivity::class.java) //가입 화면
+        val home_intent = Intent(this, HomeActivity::class.java) //홈 화면
 
         //hide navigation bar
         window.decorView.apply{
             systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
 
         }
-
-        val join_intent = Intent(this, JoinActivity::class.java) //가입 화면
-        val home_intent = Intent(this, HomeActivity::class.java)
 
         val db = FirebaseFirestore.getInstance()
         val collectionRef = db.collection("User")
@@ -110,7 +111,6 @@ class LoginActivity : ComponentActivity() {
                 // 로그인 실패 부분
                 if (error != null) {
                     Log.e(TAG, "로그인 실패 $error")
-                    // 사용자가 취소
                     if (error is ClientError && error.reason == ClientErrorCause.Cancelled ) {
                         return@loginWithKakaoTalk
                     }
@@ -122,11 +122,17 @@ class LoginActivity : ComponentActivity() {
                 // 로그인 성공 부분
                 else if (token != null) {
                     Log.e(TAG, "로그인 성공 ${token.accessToken}")
+                    token?.accessToken?.let {
+                        Log.d("DEBUG", "카카오톡 토큰 $it")
+                        loginInFirebase()
+                    }
                     val home_intent = Intent(this, HomeActivity::class.java)
                     startActivity(home_intent)
                 }
             }
-        } else {
+        }
+        //카카오계정 로그인(카카오톡 설치가 안 되어있을 시)
+        else {
             UserApiClient.instance.loginWithKakaoAccount(this, callback = mCallback) // 카카오 이메일 로그인
         }
         if (AuthApiClient.instance.hasToken()) {
@@ -145,6 +151,38 @@ class LoginActivity : ComponentActivity() {
                 Log.e(TAG, "사용자 정보 요청 실패 $error")
             } else if (user != null) {
                 Log.e(TAG, "사용자 정보 요청 성공 : $user")
+            }
+        }
+    }
+
+    private fun loginInFirebase() {
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                Log.e("DEBUG", "카카오톡 사용자 정보가져오기 에러 ${error.message}")
+            } else {
+                Log.d("DEBUG", "카카오톡 사용자 정보가져오기 success.")
+                // 파이어베이스 유저 생성 (이메일로 회원가입)
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    user?.kakaoAccount?.email ?: "",
+                    "${user?.id}"
+                ).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d("DEBUG", "파이어베이스 사용자 생성")
+                        // 회원가입 화면으로 이동
+                        val join_intent = Intent(this, JoinActivity::class.java) //가입 화면
+                        startActivity(join_intent)
+                        // 창닫기
+                        finish()
+                    } else {
+                        Log.e("DEBUG", "파이어베이스 사용자 생성 실패 ${task.exception?.message}")
+                        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                            user?.kakaoAccount?.email ?: "",
+                            "${user?.id}"
+                        )
+                        val join_intent = Intent(this, JoinActivity::class.java) //가입 화면
+                        startActivity(join_intent)
+                    }
+                }
             }
         }
     }
